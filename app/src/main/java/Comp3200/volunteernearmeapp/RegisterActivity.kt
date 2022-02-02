d71e88c5c8f12b9e5978946d028d4ce646a7a8ce
Constantinos <cp1g19@soton.ac.uk>
2022-02-02 01:13:32 +0000

    Fixed sign up

--- app/src/main/java/Comp3200/volunteernearmeapp/RegisterActivity.kt
+++ app/src/main/java/Comp3200/volunteernearmeapp/RegisterActivity.kt
@@ -1,14 +1,19 @@
 package Comp3200.volunteernearmeapp
 
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.util.Patterns
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
@@ -17,6 +22,10 @@ import com.google.firebase.ktx.Firebase
 class RegisterActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var fStore: FirebaseFirestore
+    private lateinit var database: DatabaseReference
+    var userId = ""
+    private val TAG = "RegisterActivity"
+    private var role: String? = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -26,7 +35,7 @@ class RegisterActivity : AppCompatActivity() {
         auth = Firebase.auth
         fStore = Firebase.firestore
         val tickBox: CheckBox= findViewById(R.id.checkbox)
-        tickBox.setOnClickListener{
+        tickBox.setOnClickListener {
             val orgName: EditText = findViewById(R.id.et_organization_name)
             orgName.isVisible = true
         }
@@ -37,79 +46,70 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun registerUser() {
-//        val emailInfo: TextView = findViewById(R.id.et_username);
-//        val password: TextView = findViewById(R.id.et_password);
-//
-//
-//        //check if empty email
-//        if (emailInfo.text.toString().isEmpty()) {
-//            emailInfo.error = "Please enter email"
-//            emailInfo.requestFocus()
-//            return
-//        }
-//
-//        //check if email is valid
-//        if (!Patterns.EMAIL_ADDRESS.matcher(username.text.toString()).matches()) {
-//            emailInfo.error = "Please enter a valid email"
-//            emailInfo.requestFocus()
-//            return
-//        }
-//
-//        //check if a password empty
-//        if (password.text.toString().isEmpty()) {
-//            password.error = "Please enter password"
-//            password.requestFocus()
-//            return
-//        }
-//
-//
-//        //register user
-//        auth.createUserWithEmailAndPassword(emailInfo.text.toString(), password.text.toString())
-//            .addOnCompleteListener(this) { task ->
-//                if (task.isSuccessful) {
-//                    // Sign in success, update UI with the signed-in user's information
-//                    val user = Firebase.auth.currentUser
-//
-//                    //send a verification email to the registered email ID
-//                    user!!.sendEmailVerification()
-//                        .addOnCompleteListener { task ->
-//                            if (task.isSuccessful) {
-//                                Toast.makeText(baseContext,"Signed up successfuly. " +
-//                                        "A verification email has been sent to your email address",
-//                                    Toast.LENGTH_SHORT).show()
-//                                userID = user.uid
-//                                if(username.text.toString() == "ts1g18@soton.ac.uk"){
-//                                    role = "Team Captain"
-//                                }else{
-//                                    role = "Team Member"
-//                                }
-//                                // Create a new user with a fullname and username(emailID) and student id
-//                                val user = hashMapOf(
-//                                    "Email ID" to emailInfo.text.toString(),
-//                                    "Role" to role
-//                                )
-//                                val documentReference: DocumentReference =
-//                                    fStore.collection("users").document(userID)
-//                                // Add a new document with a generated ID
-//                                documentReference.set(user)
-//                                    .addOnSuccessListener { documentReference ->
-//                                        Log.d(TAG, "DocumentSnapshot added with ID: $userID")
-//                                    }
-//                                    .addOnFailureListener { e ->
-//                                        Log.w(TAG, "Error adding document", e)
-//                                    }
-//                                startActivity(Intent(this, LoginActivity::class.java))
-//                                finish()
-//                            }
-//                        }
-//                } else {
-//                    // If sign in fails, display a message to the user.
-//                    Toast.makeText(
-//                        baseContext, "Sign up failed. Try again later.",
-//                        Toast.LENGTH_SHORT
-//                    ).show()
-//                }
-//            }
+        val emailInfo: TextView = findViewById(R.id.et_username);
+        val password: TextView = findViewById(R.id.et_password);
+        if (emailInfo.text.toString().isEmpty()) {
+            emailInfo.error = "Please enter email"
+            emailInfo.requestFocus()
+            return
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailInfo.text.toString()).matches()) {
+            emailInfo.error = "Please enter a valid email"
+            emailInfo.requestFocus()
+            return
+        }
+        if (password.text.toString().isEmpty()) {
+            password.error = "Please enter password"
+            password.requestFocus()
+            return
+        }
+        auth.createUserWithEmailAndPassword(emailInfo.text.toString(), password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = Firebase.auth.currentUser
+                    user!!.sendEmailVerification()
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(baseContext,"Registered successfuly. " + "Verification email has been sent!", Toast.LENGTH_SHORT).show()
+
+                                userId = user.uid
+                                val tickBox: CheckBox= findViewById(R.id.checkbox)
+                                val orgName: EditText = findViewById(R.id.et_organization_name)
+                                database = Firebase.database.reference
+                                if(tickBox.isChecked && !orgName.text.isEmpty()){
+                                    role = "Organizer"
+                                    database.child("users").child(userId).setValue("Organizer")
+                                }
+
+                                else{
+                                    role = "Volunteer"
+                                    database.child("users").child(userId).setValue("Volunteer")
+                                }
+                                // Create a new user with a role email
+                                val user = hashMapOf(
+                                    "Email ID" to emailInfo.text.toString(),
+                                    "Role" to role
+                                )
+                                val documentReference: DocumentReference =
+                                    fStore.collection("users").document(userId)
+                                // Add a new document with a generated ID
+                                documentReference.set(user).addOnSuccessListener { documentReference ->
+                                        Log.d(TAG, "DocumentSnapshot added with ID: $userId")
+                                    }
+                                    .addOnFailureListener { e ->
+                                        Log.w(TAG, "Error adding document", e)
+                                    }
+                                startActivity(Intent(this, LoginActivity::class.java))
+                                finish()
+                            }
+                        }
+                } else {
+                    Toast.makeText(
+                        baseContext, "Sign up failed. Try again later.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 
 }
